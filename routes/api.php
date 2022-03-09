308ee2225db2301d45d6788c2f379902b208055b
NTDung1401 <ntdung,.dut@gmail.com>
2022-03-09 16:05:21 +0700

    add auth & jwt

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,16 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+    Route::post('/login', 'Auth\AuthController@login');
+    Route::post('/register', 'Auth\AuthController@register');
+    Route::post('/logout', 'Auth\AuthController@logout');
+    Route::post('/refresh', 'Auth\AuthController@refresh');
+    Route::get('/user-profile', 'Auth\AuthController@userProfile');
+    Route::post('/change-pass', 'Auth\AuthController@changePassWord');    
+});
